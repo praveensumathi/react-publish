16ab9bf1062d2a87d596425d8140c4912b7dd8d0
praveensumathi <praveensumathi98@gmail.com>
2023-05-03 20:16:27 +0530

    Added Button component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Button from "./Button";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -16,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      Praveen
+      <Button></Button>
     </>
   );
 }
---
+++ src/Button.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Button() {
+  return <div>From Button Component</div>;
+}
+
+export default Button;
